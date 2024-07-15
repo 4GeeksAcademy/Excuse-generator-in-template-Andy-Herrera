755b01448985e1b7a73f171217bf59ea4886a1ef
Andy01hb <128887067+Andy01hb@users.noreply.github.com>
2024-07-14 20:13:28 -0500

    Corrección para facilitar lectura-Coding Guidelines Challenge
    Coding Guidelines Challenge
    se agregaron notas descriptivas que facilitan la lectura del código

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+// Añade un event listener al botón con el id 'generate' para que ejecute la función 'generateExcuse' cuando se haga clic en él.
 document.getElementById('generate').addEventListener('click', generateExcuse);
 
+// Función para generar una excusa aleatoria
 function generateExcuse() {
+    // Definición de arrays con posibles partes de la excusa
     const subjects = ["My dog", "My grandma", "The postman", "An alien"];
     const actions = ["ate", "lost", "destroyed", "hid"];
     const objects = ["my homework", "my keys", "the remote control", "my car"];
 
+    // Selección aleatoria de cada parte de la excusa
     const subject = subjects[Math.floor(Math.random() * subjects.length)];
     const action = actions[Math.floor(Math.random() * actions.length)];
     const object = objects[Math.floor(Math.random() * objects.length)];
 
+    // Creación de la excusa concatenando las partes seleccionadas
     const excuse = `${subject} ${action} ${object}.`;
+
+    // Muestra la excusa generada en el elemento con el id 'excuse'
     document.getElementById('excuse').innerText = excuse;
 }
